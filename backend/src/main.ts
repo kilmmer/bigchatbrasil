d371f9ee9ae84bcd661eb60a4e08304e127897e7
kilmmer <kilmmer@portaldoseller.com.br>
2023-09-15 23:30:42 -0300

    Ajustes para a execução do projeto no Docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
+import { ValidationPipe } from "@nestjs/common"
 
 // referência Swagger: https://www.prisma.io/blog/nestjs-prisma-rest-api-7D056s1BmOL0#set-up-swagger
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule)
 
-  app.enableCors();
+	app.enableCors()
 
-  const config = new DocumentBuilder()
-    .setTitle('Big Chat Brasil - Kilmmer')
-    .setDescription('Teste técnico Grupo Irrah')
-    .setVersion('0.1')
-    .build();
+	const config = new DocumentBuilder()
+		.setTitle("Big Chat Brasil - Kilmmer")
+		.setDescription("Teste técnico Grupo Irrah")
+		.setVersion("0.1")
+		.build()
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup("api", app, document)
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
-  await app.listen(process.env.PORT);
+	console.log(process.env.PORT)
+	await app.listen(process.env.PORT)
 }
-bootstrap();
+bootstrap()
